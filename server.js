7ee0c938a2c34ccc2d6099e1f3a0bd578b0c5ff7
oliveruy11 <157074932+oliveruy11@users.noreply.github.com>
2024-03-07 15:54:38 -0500

    remove gitignore files

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // global variables
 require('dotenv').config() // secret values from .env file
+const mongoose = require('mongoose')
 const bill_routes = require('./routes/bills')
 const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI
 
 // initialize express app
 const express = require('express')
@@ -20,7 +22,13 @@ app.get('/', (req, res) => {
 
 app.use('/api/bills', bill_routes)
 
-// listen for requests
-app.listen(PORT, () => {
-  console.log("listening on port", PORT)
-})
+
+// connect to db
+mongoose.connect(MONGO_URI)
+  .then(() => {
+      app.listen(PORT, () => {
+        console.log("connected to mongoDB and listening on port", PORT)
+      })
+
+  })
+  .catch((error) => {console.log(error)})
